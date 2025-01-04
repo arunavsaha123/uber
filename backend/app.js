aa667445a93959ea882bacdfcf6f6ff02893696b
arunavsaha123 <arunavsaha984@gmail.com>
2025-01-04 20:35:18 +0530

    first commit

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const app = express();
+const dotenv = require('dotenv');
+dotenv.config();
+const cors = require('cors');
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Hello, World!');
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 const http = require('http');
 const app = require('./app');
+const port = process.env.PORT ||3000;
 
 const server = http.createServer(app);
 
-server.listen(3000, () => {
-    console.log('Server is running on port 3000');
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
