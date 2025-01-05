e0c77da194e883c17b29b0b233c210d6d79e271f
arunavsaha123 <arunavsaha984@gmail.com>
2025-01-05 21:15:52 +0530

    first commit

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,5 +1,22 @@
 const userModel = require('../models/user.models');
+const userService = require('../services/user.service');
+const {validationResult} = require('express-validation')
 
 module.exports.registerUser = async(req, res, next)=>{
-    
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {firstName, lastName, email, password} = req.body;
+
+    const hashPassword = await userModel.hashPassword(password);
+
+    const user = await userService.createUser({
+        firstName,
+        lastName,
+        email,
+        password: hashPassword,
+    })
 }
