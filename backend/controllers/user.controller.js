1a317d407caf2b90691fbeeeba4ea6c3f61221c7
arunavsaha123 <arunavsaha984@gmail.com>
2025-01-06 13:55:51 +0530

    first commit

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -23,3 +23,19 @@ module.exports.registerUser = async (req, res, next) => {
     const token = user.generateAuthToken();
     res.status(201).json({ user, token });
 }
+
+module.exports.loginUser = async (req, res, next) => {
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    
+    const { email, password } = req.body;
+
+    const user = await userModel.findOne({email}).select('+password');
+    
+    if (!user) {
+        return res.status(401).json({ error: 'Invalid email or password' });
+    }
+}
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -11,4 +11,11 @@ Router.post('/register',[
 userController.registerUser
 )
 
+Router.post('/login',[
+    body('email').isEmail().withMessage('Please enter a valid email'),
+    body('password').isLength({min: 6}).withMessage('Password must be at least 6 characters long')
+],
+userController.loginUser
+)
+
 module.exports = Router;
