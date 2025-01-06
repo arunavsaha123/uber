265e2e9a32009ee5598a37d4d3a36597e4b720b6
arunavsaha123 <arunavsaha984@gmail.com>
2025-01-06 18:12:23 +0530

    first commit

--- backend/app.js
+++ backend/app.js
@@ -5,12 +5,14 @@ dotenv.config();
 const cors = require('cors');
 const connecttoDb = require('./db/db');
 const userRoutes = require('./routes/user.routes')
+const cookieparser = require('cookie-parser');
 
 connecttoDb();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieparser());
 
 app.get('/', (req, res) => {
     res.send('Hello, World!');
