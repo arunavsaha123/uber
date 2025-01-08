ac7528dc3cc550839b2cfed3da0c95636b0f0bfc
arunavsaha123 <arunavsaha984@gmail.com>
2025-01-08 20:21:25 +0530

    first commit

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -12,6 +12,12 @@ module.exports.registerUser = async (req, res, next) => {
 
     const { fullname, email, password } = req.body;
 
+    const isUserAlready = await userModel.findOne({ email });
+    
+    if (isUserAlready) {
+        return res.status(409).json({ error: 'Email already exists' });
+    }
+
     const hashPassword = await userModel.hashPassword(password);
 
     const user = await userService.createUser({
--- backend/routes/captain.routes.js
+++ backend/routes/captain.routes.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const {body} =require("express-validator")
+const captainController = require('../controllers/captain.controller')
 
 router.post('/register',[
     body('fullname.firstname').isLength({min:3}).withMessage('First name should be 3 characters long'),
     body('email').isEmail().withMessage('Please enter a valid email'),
     body('password').isLength({min: 6}).withMessage('Password must be at least 6 characters long'),
-    body(vehicle.colour).isLength({min: 3}).withMessage('colour must be atleast 3 characters long'),
-    body(vehicle.plate).isLength({min: 3}).withMessage('plate must be atleast 3 characters long'),
-    body(vehicle.capacity).isInt({min: 1}).withMessage('capacity must be atleast 1'),
-    body(vehicle.vehicleType).isIn('car','motorcycle','auto').withMessage('invalid vehicle type'),
+    body('vehicle.color').isLength({min: 3}).withMessage('color must be atleast 3 characters long'),
+    body('vehicle.plate').isLength({min: 3}).withMessage('plate must be atleast 3 characters long'),
+    body('vehicle.capacity').isInt({min: 1}).withMessage('capacity must be atleast 1'),
+    body('vehicle.vehicleType').isIn('car','motorcycle','auto').withMessage('invalid vehicle type'),
 ],
-    userController.registerUser
+    captainController.registerCaptain
 )
 
 
